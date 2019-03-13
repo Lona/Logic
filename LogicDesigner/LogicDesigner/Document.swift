545ac03f4994b1a6c29d19aed4b96300eafce956
Devin Abbott <devinabbott@gmail.com>
2019-03-13 16:36:34 -0700

    Add logic file reading and writing

--- LogicDesigner/LogicDesigner/Document.swift
+++ LogicDesigner/LogicDesigner/Document.swift
@@ -294,18 +294,14 @@ class Document: NSDocument {
     }
 
     override func data(ofType typeName: String) throws -> Data {
-        // Insert code here to write your document to data of the specified type, throwing an error in case of failure.
-        // Alternatively, you could remove this method and override fileWrapper(ofType:), write(to:ofType:), or write(to:ofType:for:originalContentsURL:) instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
+
+        return try encoder.encode(rootNode)
     }
 
     override func read(from data: Data, ofType typeName: String) throws {
-        // Insert code here to read your document from the given data of the specified type, throwing an error in case of failure.
-        // Alternatively, you could remove this method and override read(from:ofType:) instead.
-        // If you do, you should also override isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        rootNode = try JSONDecoder().decode(SwiftSyntaxNode.self, from: data)
     }
-
-
 }
 
