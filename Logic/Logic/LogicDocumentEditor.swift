696eb4cba217368d50d06d45efe95b9aee4fc775
Devin Abbott <devinabbott@gmail.com>
2019-03-14 20:06:15 -0700

    Wrap editor logic in LogicDocumentEditor

---
+++ Logic/Logic/LogicDocumentEditor.swift
@@ -0,0 +1,286 @@
+import AppKit
+
+public let defaultRootNode = SwiftSyntaxNode.program(
+    SwiftProgram(
+        id: NSUUID().uuidString,
+        block: SwiftList<SwiftStatement>.next(
+            SwiftStatement.placeholderStatement(
+                SwiftPlaceholderStatement(id: NSUUID().uuidString)
+            ),
+            .empty
+        )
+    )
+)
+
+// MARK: - LogicDocumentEditor
+
+public class LogicDocumentEditor: NSBox {
+
+    // MARK: Lifecycle
+
+    public init(rootNode: SwiftSyntaxNode = defaultRootNode) {
+        self.rootNode = rootNode
+
+        super.init(frame: .zero)
+
+        setUpViews()
+        setUpConstraints()
+
+        update()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Public
+
+    public var rootNode: SwiftSyntaxNode
+
+    var childWindow: SuggestionWindow? = SuggestionWindow()
+
+    var suggestionText: String = "" {
+        didSet {
+            childWindow?.suggestionText = suggestionText
+        }
+    }
+
+    var selectedSuggestionIndex: Int? {
+        didSet {
+            childWindow?.selectedIndex = selectedSuggestionIndex
+        }
+    }
+
+    func indexedSuggestionListItems(for logicSuggestionItems: [LogicSuggestionItem]) -> [(offset: Int, item: SuggestionListItem)] {
+        var categories: [(name: String, list: [(offset: Int, item: LogicSuggestionItem)])] = []
+
+        logicSuggestionItems.enumerated().forEach { offset, item in
+            if let categoryIndex = categories.firstIndex(where: { $0.name == item.category }) {
+                let category = categories[categoryIndex]
+                categories[categoryIndex] = (category.name, category.list + [(offset, item)])
+            } else {
+                categories.append((item.category, [(offset, item)]))
+            }
+        }
+
+        var suggestionListItems: [(Int, SuggestionListItem)] = []
+
+        categories.forEach { category in
+            suggestionListItems.append((0, .sectionHeader(category.name)))
+
+            category.list.forEach { logicItem in
+                suggestionListItems.append((logicItem.offset, .row(logicItem.item.title, logicItem.item.disabled)))
+            }
+        }
+
+        return suggestionListItems
+    }
+
+    func logicSuggestionItems(for syntaxNode: SwiftSyntaxNode, prefix: String) -> [LogicSuggestionItem] {
+        guard let range = rootNode.elementRange(for: syntaxNode.uuid),
+            let elementPath = rootNode.pathTo(id: syntaxNode.uuid) else { return [] }
+
+        let highestMatch = elementPath.first(where: { rootNode.elementRange(for: $0.uuid) == range }) ?? syntaxNode
+
+        return highestMatch.suggestions(for: prefix)
+    }
+
+    func nextNode() {
+        if let index = self.logicEditor.nextActivatableIndex(after: self.logicEditor.selectedRange?.lowerBound),
+            let id = self.rootNode.formatted.elements[index].syntaxNodeID {
+
+            self.logicEditor.selectedRange = self.rootNode.elementRange(for: id)
+            self.suggestionText = ""
+
+            let nextSyntaxNode = self.rootNode.topNodeWithEqualElements(as: id)
+            self.showSuggestionWindow(for: index, syntaxNode: nextSyntaxNode)
+        } else {
+            Swift.print("No next node to activate")
+
+            self.hideSuggestionWindow()
+        }
+    }
+
+    func previousNode() {
+        if let index = self.logicEditor.previousActivatableIndex(before: self.logicEditor.selectedRange?.lowerBound),
+            let id = self.rootNode.formatted.elements[index].syntaxNodeID {
+
+            self.logicEditor.selectedRange = self.rootNode.elementRange(for: id)
+            self.suggestionText = ""
+
+            let nextSyntaxNode = self.rootNode.topNodeWithEqualElements(as: id)
+            self.showSuggestionWindow(for: index, syntaxNode: nextSyntaxNode)
+        } else {
+            Swift.print("No previous node to activate")
+
+            self.hideSuggestionWindow()
+        }
+    }
+
+    func showSuggestionWindow(for nodeIndex: Int, syntaxNode: SwiftSyntaxNode) {
+        guard let window = self.window, let childWindow = self.childWindow else { return }
+
+        let syntaxNodePath = self.rootNode.uniqueElementPathTo(id: syntaxNode.uuid)
+        let dropdownNodes = Array(syntaxNodePath)
+
+        var logicSuggestions = self.logicSuggestionItems(for: syntaxNode, prefix: suggestionText)
+
+        let originalIndexedSuggestions = indexedSuggestionListItems(for: logicSuggestions)
+        childWindow.detailView = logicSuggestions.first?.node.documentation(for: suggestionText).makeScrollView()
+        childWindow.suggestionItems = originalIndexedSuggestions.map { $0.item }
+        childWindow.selectedIndex = originalIndexedSuggestions.firstIndex { $0.item.isSelectable }
+        childWindow.dropdownValues = dropdownNodes.map { $0.nodeTypeDescription }
+        childWindow.dropdownIndex = dropdownNodes.count - 1
+
+        childWindow.onSelectIndex = { index in
+            self.selectedSuggestionIndex = index
+
+            if let index = index {
+                let indexedSuggestions = self.indexedSuggestionListItems(for: logicSuggestions)
+                let suggestedNode = logicSuggestions[indexedSuggestions[index].offset].node
+
+                childWindow.detailView = suggestedNode.documentation(for: self.suggestionText).makeScrollView()
+            } else {
+                childWindow.detailView = nil
+            }
+        }
+
+        childWindow.onPressEscapeKey = {
+            self.hideSuggestionWindow()
+        }
+
+        childWindow.onPressTabKey = self.nextNode
+
+        childWindow.onPressShiftTabKey = self.previousNode
+
+        childWindow.onHighlightDropdownIndex = { highlightedIndex in
+            if let highlightedIndex = highlightedIndex {
+                let selected = dropdownNodes[highlightedIndex]
+                let range = self.rootNode.elementRange(for: selected.uuid)
+
+                self.logicEditor.outlinedRange = range
+            } else {
+                self.logicEditor.outlinedRange = nil
+            }
+        }
+
+        childWindow.onSelectDropdownIndex = { selectedIndex in
+            self.logicEditor.outlinedRange = nil
+            self.select(nodeByID: dropdownNodes[selectedIndex].uuid)
+        }
+
+        childWindow.onSubmit = { index in
+            let indexedSuggestions = self.indexedSuggestionListItems(for: logicSuggestions)
+            let logicSuggestionItem = logicSuggestions[indexedSuggestions[index].offset]
+
+            if logicSuggestionItem.disabled { return }
+
+            let suggestedNode = logicSuggestionItem.node
+
+            Swift.print("Chose suggestion", suggestedNode)
+
+            let replacement = self.rootNode.replace(id: syntaxNode.uuid, with: suggestedNode)
+
+            self.rootNode = replacement
+
+            self.logicEditor.formattedContent = self.rootNode.formatted
+
+            if suggestedNode.movementAfterInsertion == .next {
+                self.nextNode()
+            } else {
+                self.handleActivateElement(self.logicEditor.selectedRange?.lowerBound)
+            }
+        }
+
+        childWindow.onChangeSuggestionText = { value in
+            self.suggestionText = value
+
+            // Update logicSuggestions
+            logicSuggestions = self.logicSuggestionItems(for: syntaxNode, prefix: value)
+
+            let indexedSuggestions = self.indexedSuggestionListItems(for: logicSuggestions)
+            childWindow.suggestionItems = indexedSuggestions.map { $0.item }
+            childWindow.selectedIndex = indexedSuggestions.firstIndex(where: { $0.item.isSelectable })
+        }
+
+        window.addChildWindow(childWindow, ordered: .above)
+
+        if let rect = self.logicEditor.getBoundingRect(for: nodeIndex) {
+            let screenRect = window.convertToScreen(rect)
+            childWindow.anchorTo(rect: screenRect)
+            childWindow.focusSearchField()
+        }
+    }
+
+    func hideSuggestionWindow() {
+        guard let window = self.window, let childWindow = self.childWindow else { return }
+
+        window.removeChildWindow(childWindow)
+        childWindow.setIsVisible(false)
+    }
+
+    func select(nodeByID syntaxNodeId: SwiftUUID?) {
+        self.logicEditor.selectedLine = nil
+        self.suggestionText = ""
+
+        if let syntaxNodeId = syntaxNodeId {
+            let topNode = self.rootNode.topNodeWithEqualElements(as: syntaxNodeId)
+
+            if let selectedRange = self.rootNode.elementRange(for: topNode.uuid) {
+                self.logicEditor.selectedRange = selectedRange
+                self.showSuggestionWindow(for: selectedRange.lowerBound, syntaxNode: topNode)
+            } else {
+                self.logicEditor.selectedRange = nil
+                self.hideSuggestionWindow()
+            }
+        } else {
+            self.logicEditor.selectedRange = nil
+            self.hideSuggestionWindow()
+        }
+    }
+
+    // MARK: Private
+
+    private let logicEditor = LogicEditor()
+
+    func handleActivateElement(_ activatedIndex: Int?) {
+        if let activatedIndex = activatedIndex {
+            let id = self.rootNode.formatted.elements[activatedIndex].syntaxNodeID
+            self.select(nodeByID: id)
+        } else {
+            self.select(nodeByID: nil)
+        }
+    }
+
+    func handleActivateLine(_ activatedLineIndex: Int) {
+        handleActivateElement(nil)
+
+        Swift.print("Activate line \(activatedLineIndex)")
+
+        logicEditor.selectedLine = activatedLineIndex
+    }
+
+    private func setUpViews() {
+        boxType = .custom
+        borderType = .noBorder
+        contentViewMargins = .zero
+
+        addSubview(logicEditor)
+
+        logicEditor.formattedContent = rootNode.formatted
+        logicEditor.onActivate = handleActivateElement
+        logicEditor.onActivateLine = handleActivateLine
+    }
+
+    private func setUpConstraints() {
+        translatesAutoresizingMaskIntoConstraints = false
+        logicEditor.translatesAutoresizingMaskIntoConstraints = false
+        
+        logicEditor.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        logicEditor.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        logicEditor.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        logicEditor.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+
+    private func update() {}
+}
--- LogicDesigner/LogicDesigner/Document.swift
+++ LogicDesigner/LogicDesigner/Document.swift
@@ -13,254 +13,15 @@ class Document: NSDocument {
 
     override init() {
         super.init()
-//        testFormatter()
     }
 
     override class var autosavesInPlace: Bool {
         return false
     }
 
-    var rootNode: SwiftSyntaxNode = SwiftSyntaxNode.program(
-        SwiftProgram(
-            id: NSUUID().uuidString,
-            block: SwiftList<SwiftStatement>.next(
-                SwiftStatement.placeholderStatement(
-                    SwiftPlaceholderStatement(id: NSUUID().uuidString)
-                ),
-                .empty
-            )
-        )
-    )
-
-    var suggestionText: String = "" {
-        didSet {
-            childWindow?.suggestionText = suggestionText
-        }
-    }
-
-    var selectedSuggestionIndex: Int? {
-        didSet {
-            childWindow?.selectedIndex = selectedSuggestionIndex
-        }
-    }
-
-    func indexedSuggestionListItems(for logicSuggestionItems: [LogicSuggestionItem]) -> [(offset: Int, item: SuggestionListItem)] {
-        var categories: [(name: String, list: [(offset: Int, item: LogicSuggestionItem)])] = []
-
-        logicSuggestionItems.enumerated().forEach { offset, item in
-            if let categoryIndex = categories.firstIndex(where: { $0.name == item.category }) {
-                let category = categories[categoryIndex]
-                categories[categoryIndex] = (category.name, category.list + [(offset, item)])
-            } else {
-                categories.append((item.category, [(offset, item)]))
-            }
-        }
-
-        var suggestionListItems: [(Int, SuggestionListItem)] = []
-
-        categories.forEach { category in
-            suggestionListItems.append((0, .sectionHeader(category.name)))
-
-            category.list.forEach { logicItem in
-                suggestionListItems.append((logicItem.offset, .row(logicItem.item.title, logicItem.item.disabled)))
-            }
-        }
-
-        return suggestionListItems
-    }
-
-    func logicSuggestionItems(for syntaxNode: SwiftSyntaxNode, prefix: String) -> [LogicSuggestionItem] {
-        guard let range = rootNode.elementRange(for: syntaxNode.uuid),
-            let elementPath = rootNode.pathTo(id: syntaxNode.uuid) else { return [] }
-
-        let highestMatch = elementPath.first(where: { rootNode.elementRange(for: $0.uuid) == range }) ?? syntaxNode
-
-        return highestMatch.suggestions(for: prefix)
-    }
-
-    func nextNode() {
-        if let index = self.logicEditor.nextActivatableIndex(after: self.logicEditor.selectedRange?.lowerBound),
-            let id = self.rootNode.formatted.elements[index].syntaxNodeID {
-
-            self.logicEditor.selectedRange = self.rootNode.elementRange(for: id)
-            self.suggestionText = ""
-
-            let nextSyntaxNode = self.rootNode.topNodeWithEqualElements(as: id)
-            self.showSuggestionWindow(for: index, syntaxNode: nextSyntaxNode)
-        } else {
-            Swift.print("No next node to activate")
-
-            self.hideSuggestionWindow()
-        }
-    }
-
-    func previousNode() {
-        if let index = self.logicEditor.previousActivatableIndex(before: self.logicEditor.selectedRange?.lowerBound),
-            let id = self.rootNode.formatted.elements[index].syntaxNodeID {
-
-            self.logicEditor.selectedRange = self.rootNode.elementRange(for: id)
-            self.suggestionText = ""
-
-            let nextSyntaxNode = self.rootNode.topNodeWithEqualElements(as: id)
-            self.showSuggestionWindow(for: index, syntaxNode: nextSyntaxNode)
-        } else {
-            Swift.print("No previous node to activate")
-
-            self.hideSuggestionWindow()
-        }
-    }
-
-    func showSuggestionWindow(for nodeIndex: Int, syntaxNode: SwiftSyntaxNode) {
-        guard let window = self.window, let childWindow = self.childWindow else { return }
-
-        let syntaxNodePath = self.rootNode.uniqueElementPathTo(id: syntaxNode.uuid)
-        let dropdownNodes = Array(syntaxNodePath)
-
-        var logicSuggestions = self.logicSuggestionItems(for: syntaxNode, prefix: suggestionText)
-
-        let originalIndexedSuggestions = indexedSuggestionListItems(for: logicSuggestions)
-        childWindow.detailView = logicSuggestions.first?.node.documentation(for: suggestionText).makeScrollView()
-        childWindow.suggestionItems = originalIndexedSuggestions.map { $0.item }
-        childWindow.selectedIndex = originalIndexedSuggestions.firstIndex { $0.item.isSelectable }
-        childWindow.dropdownValues = dropdownNodes.map { $0.nodeTypeDescription }
-        childWindow.dropdownIndex = dropdownNodes.count - 1
-
-        childWindow.onSelectIndex = { index in
-            self.selectedSuggestionIndex = index
-
-            if let index = index {
-                let indexedSuggestions = self.indexedSuggestionListItems(for: logicSuggestions)
-                let suggestedNode = logicSuggestions[indexedSuggestions[index].offset].node
-
-                childWindow.detailView = suggestedNode.documentation(for: self.suggestionText).makeScrollView()
-            } else {
-                childWindow.detailView = nil
-            }
-        }
-
-        childWindow.onPressEscapeKey = {
-            self.hideSuggestionWindow()
-        }
-
-        childWindow.onPressTabKey = self.nextNode
-
-        childWindow.onPressShiftTabKey = self.previousNode
-
-        childWindow.onHighlightDropdownIndex = { highlightedIndex in
-            if let highlightedIndex = highlightedIndex {
-                let selected = dropdownNodes[highlightedIndex]
-                let range = self.rootNode.elementRange(for: selected.uuid)
-
-                self.logicEditor.outlinedRange = range
-            } else {
-                self.logicEditor.outlinedRange = nil
-            }
-        }
-
-        childWindow.onSelectDropdownIndex = { selectedIndex in
-            self.logicEditor.outlinedRange = nil
-            self.select(nodeByID: dropdownNodes[selectedIndex].uuid)
-        }
-
-        childWindow.onSubmit = { index in
-            let indexedSuggestions = self.indexedSuggestionListItems(for: logicSuggestions)
-            let logicSuggestionItem = logicSuggestions[indexedSuggestions[index].offset]
-
-            if logicSuggestionItem.disabled { return }
-
-            let suggestedNode = logicSuggestionItem.node
-
-            Swift.print("Chose suggestion", suggestedNode)
-
-            let replacement = self.rootNode.replace(id: syntaxNode.uuid, with: suggestedNode)
-
-            self.rootNode = replacement
-
-            self.logicEditor.formattedContent = self.rootNode.formatted
-
-            if suggestedNode.movementAfterInsertion == .next {
-                self.nextNode()
-            } else {
-                self.handleActivateElement(self.logicEditor.selectedRange?.lowerBound)
-            }
-        }
-
-        childWindow.onChangeSuggestionText = { value in
-            self.suggestionText = value
-
-            // Update logicSuggestions
-            logicSuggestions = self.logicSuggestionItems(for: syntaxNode, prefix: value)
-
-            let indexedSuggestions = self.indexedSuggestionListItems(for: logicSuggestions)
-            childWindow.suggestionItems = indexedSuggestions.map { $0.item }
-            childWindow.selectedIndex = indexedSuggestions.firstIndex(where: { $0.item.isSelectable })
-        }
-
-        window.addChildWindow(childWindow, ordered: .above)
-
-        if let rect = self.logicEditor.getBoundingRect(for: nodeIndex) {
-            let screenRect = window.convertToScreen(rect)
-            childWindow.anchorTo(rect: screenRect)
-            childWindow.focusSearchField()
-        }
-    }
-
-    func hideSuggestionWindow() {
-        guard let window = self.window, let childWindow = self.childWindow else { return }
-
-        window.removeChildWindow(childWindow)
-        childWindow.setIsVisible(false)
-    }
-
-    func select(nodeByID syntaxNodeId: SwiftUUID?) {
-        self.logicEditor.selectedLine = nil
-        self.suggestionText = ""
-
-        if let syntaxNodeId = syntaxNodeId {
-            let topNode = self.rootNode.topNodeWithEqualElements(as: syntaxNodeId)
-
-            if let selectedRange = self.rootNode.elementRange(for: topNode.uuid) {
-                self.logicEditor.selectedRange = selectedRange
-                self.showSuggestionWindow(for: selectedRange.lowerBound, syntaxNode: topNode)
-            } else {
-                self.logicEditor.selectedRange = nil
-                self.hideSuggestionWindow()
-            }
-        } else {
-            self.logicEditor.selectedRange = nil
-            self.hideSuggestionWindow()
-        }
-    }
-
-    private let logicEditor = LogicEditor()
-
-    func handleActivateElement(_ activatedIndex: Int?) {
-        if let activatedIndex = activatedIndex {
-            let id = self.rootNode.formatted.elements[activatedIndex].syntaxNodeID
-            self.select(nodeByID: id)
-        } else {
-            self.select(nodeByID: nil)
-        }
-    }
-
-    func handleActivateLine(_ activatedLineIndex: Int) {
-        handleActivateElement(nil)
-
-        Swift.print("Activate line \(activatedLineIndex)")
-
-        logicEditor.selectedLine = activatedLineIndex
-    }
-
-    func setUpViews() -> NSView {
-        logicEditor.formattedContent = rootNode.formatted
-        logicEditor.onActivate = handleActivateElement
-        logicEditor.onActivateLine = handleActivateLine
-
-        return logicEditor
-    }
-
-    var childWindow: SuggestionWindow?
     var window: NSWindow?
+
+    var logicDocumentEditor = LogicDocumentEditor()
 
     override func makeWindowControllers() {
         let window = NSWindow(
@@ -270,39 +31,25 @@ class Document: NSDocument {
             defer: false)
 
         window.backgroundColor = NSColor.white
-
         window.center()
-
-        window.contentView = setUpViews()
-
-        let windowController = NSWindowController(window: window)
-
-        windowController.showWindow(nil)
-
-        addWindowController(windowController)
+        window.contentView = logicDocumentEditor
 
         self.window = window
 
-        let childWindow = SuggestionWindow()
-
-        self.childWindow = childWindow
-    }
-
-    override var windowNibName: NSNib.Name? {
-        // Returns the nib file name of the document
-        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
-        return NSNib.Name("Document")
+        let windowController = NSWindowController(window: window)
+        windowController.showWindow(nil)
+        addWindowController(windowController)
     }
 
     override func data(ofType typeName: String) throws -> Data {
         let encoder = JSONEncoder()
         encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
 
-        return try encoder.encode(rootNode)
+        return try encoder.encode(logicDocumentEditor.rootNode)
     }
 
     override func read(from data: Data, ofType typeName: String) throws {
-        rootNode = try JSONDecoder().decode(SwiftSyntaxNode.self, from: data)
+        logicDocumentEditor.rootNode = try JSONDecoder().decode(SwiftSyntaxNode.self, from: data)
     }
 }
 
