63eab4d05f9d6bf236923d8ef139f48ceb9b07f5
Devin Abbott <devinabbott@gmail.com>
2019-03-16 10:44:58 -0700

    Improved generated types

--- Logic/Logic/Editor/Syntax/LGCSyntax.swift
+++ Logic/Logic/Editor/Syntax/LGCSyntax.swift
@@ -10,200 +10,6 @@ public struct LGCIdentifier: Codable & Equatable {
   public var string: String
 }
 
-public struct LGCLoop: Codable & Equatable {
-  public init(pattern: LGCPattern, expression: LGCExpression, block: LGCList<LGCStatement>, id: UUID) {
-    self.pattern = pattern
-    self.expression = expression
-    self.block = block
-    self.id = id
-  }
-
-  public var pattern: LGCPattern
-  public var expression: LGCExpression
-  public var block: LGCList<LGCStatement>
-  public var id: UUID
-}
-
-public struct LGCBranch: Codable & Equatable {
-  public init(id: UUID, condition: LGCExpression, block: LGCList<LGCStatement>) {
-    self.id = id
-    self.condition = condition
-    self.block = block
-  }
-
-  public var id: UUID
-  public var condition: LGCExpression
-  public var block: LGCList<LGCStatement>
-}
-
-public struct LGCDecl: Codable & Equatable {
-  public init(content: LGCDeclaration, id: UUID) {
-    self.content = content
-    self.id = id
-  }
-
-  public var content: LGCDeclaration
-  public var id: UUID
-}
-
-public struct LGCExpressionStatement: Codable & Equatable {
-  public init(id: UUID, expression: LGCExpression) {
-    self.id = id
-    self.expression = expression
-  }
-
-  public var id: UUID
-  public var expression: LGCExpression
-}
-
-public struct LGCPlaceholderStatement: Codable & Equatable {
-  public init(id: UUID) {
-    self.id = id
-  }
-
-  public var id: UUID
-}
-
-public struct LGCVariable: Codable & Equatable {
-  public init(id: UUID) {
-    self.id = id
-  }
-
-  public var id: UUID
-}
-
-public struct LGCFunction: Codable & Equatable {
-  public init(id: UUID) {
-    self.id = id
-  }
-
-  public var id: UUID
-}
-
-public struct LGCBinaryExpression: Codable & Equatable {
-  public init(left: LGCExpression, right: LGCExpression, op: LGCBinaryOperator, id: UUID) {
-    self.left = left
-    self.right = right
-    self.op = op
-    self.id = id
-  }
-
-  public var left: LGCExpression
-  public var right: LGCExpression
-  public var op: LGCBinaryOperator
-  public var id: UUID
-}
-
-public struct LGCIdentifierExpression: Codable & Equatable {
-  public init(id: UUID, identifier: LGCIdentifier) {
-    self.id = id
-    self.identifier = identifier
-  }
-
-  public var id: UUID
-  public var identifier: LGCIdentifier
-}
-
-public struct LGCFunctionCallExpression: Codable & Equatable {
-  public init(id: UUID, expression: LGCExpression, arguments: LGCList<LGCFunctionCallArgument>) {
-    self.id = id
-    self.expression = expression
-    self.arguments = arguments
-  }
-
-  public var id: UUID
-  public var expression: LGCExpression
-  public var arguments: LGCList<LGCFunctionCallArgument>
-}
-
-public struct LGCPattern: Codable & Equatable {
-  public init(id: UUID, name: String) {
-    self.id = id
-    self.name = name
-  }
-
-  public var id: UUID
-  public var name: String
-}
-
-public struct LGCIsEqualTo: Codable & Equatable {
-  public init(id: UUID) {
-    self.id = id
-  }
-
-  public var id: UUID
-}
-
-public struct LGCIsNotEqualTo: Codable & Equatable {
-  public init(id: UUID) {
-    self.id = id
-  }
-
-  public var id: UUID
-}
-
-public struct LGCIsLessThan: Codable & Equatable {
-  public init(id: UUID) {
-    self.id = id
-  }
-
-  public var id: UUID
-}
-
-public struct LGCIsGreaterThan: Codable & Equatable {
-  public init(id: UUID) {
-    self.id = id
-  }
-
-  public var id: UUID
-}
-
-public struct LGCIsLessThanOrEqualTo: Codable & Equatable {
-  public init(id: UUID) {
-    self.id = id
-  }
-
-  public var id: UUID
-}
-
-public struct LGCIsGreaterThanOrEqualTo: Codable & Equatable {
-  public init(id: UUID) {
-    self.id = id
-  }
-
-  public var id: UUID
-}
-
-public struct LGCSetEqualTo: Codable & Equatable {
-  public init(id: UUID) {
-    self.id = id
-  }
-
-  public var id: UUID
-}
-
-public struct LGCProgram: Codable & Equatable {
-  public init(id: UUID, block: LGCList<LGCStatement>) {
-    self.id = id
-    self.block = block
-  }
-
-  public var id: UUID
-  public var block: LGCList<LGCStatement>
-}
-
-public struct LGCFunctionCallArgument: Codable & Equatable {
-  public init(id: UUID, label: String, expression: LGCExpression) {
-    self.id = id
-    self.label = label
-    self.expression = expression
-  }
-
-  public var id: UUID
-  public var label: String
-  public var expression: LGCExpression
-}
-
 public indirect enum LGCStatement: Codable & Equatable {
   case loop(pattern: LGCPattern, expression: LGCExpression, block: LGCList<LGCStatement>, id: UUID)
   case branch(id: UUID, condition: LGCExpression, block: LGCList<LGCStatement>)
@@ -515,6 +321,16 @@ public indirect enum LGCExpression: Codable & Equatable {
   }
 }
 
+public struct LGCPattern: Codable & Equatable {
+  public init(id: UUID, name: String) {
+    self.id = id
+    self.name = name
+  }
+
+  public var id: UUID
+  public var name: String
+}
+
 public indirect enum LGCBinaryOperator: Codable & Equatable {
   case isEqualTo(id: UUID)
   case isNotEqualTo(id: UUID)
@@ -588,4 +404,26 @@ public indirect enum LGCBinaryOperator: Codable & Equatable {
         try data.encode(value, forKey: .id)
     }
   }
+}
+
+public struct LGCProgram: Codable & Equatable {
+  public init(id: UUID, block: LGCList<LGCStatement>) {
+    self.id = id
+    self.block = block
+  }
+
+  public var id: UUID
+  public var block: LGCList<LGCStatement>
+}
+
+public struct LGCFunctionCallArgument: Codable & Equatable {
+  public init(id: UUID, label: String, expression: LGCExpression) {
+    self.id = id
+    self.label = label
+    self.expression = expression
+  }
+
+  public var id: UUID
+  public var label: String
+  public var expression: LGCExpression
 }
