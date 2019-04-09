94135370cbbd188b8007689e486156137ea3c0b8
Devin Abbott <devinabbott@gmail.com>
2019-04-09 14:48:00 -0700

    Deletion no longer triggers an activation

--- Logic/Logic/Editor/LogicEditor.swift
+++ Logic/Logic/Editor/LogicEditor.swift
@@ -180,7 +180,7 @@ extension LogicEditor {
         if let selectedRange = range(), let selectedNode = self.rootNode.topNodeWithEqualRange(as: selectedRange) {
             let shouldActivate = onChangeRootNode?(rootNode.delete(id: selectedNode.uuid))
             if shouldActivate == true {
-                self.handleActivateElement(selectedRange.first)
+                handleActivateElement(nil)
             }
         }
     }
