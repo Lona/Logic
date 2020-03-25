a7beb410c806365c10184b22ae2036291a564a58
Mathieu Dutour <mathieu@dutour.me>
2020-03-25 14:34:19 +0100

    fix typo

--- Logic/Logic/CodeEditor/Syntax/LGCSyntax.swift
+++ Logic/Logic/CodeEditor/Syntax/LGCSyntax.swift
@@ -1064,7 +1064,7 @@ public indirect enum LGCStatement: Codable & Equatable & Equivalentable {
         return true
       case .declaration(let value):
         return value.content.isPlaceholderNode()
-      case .expression(let value):
+      case .expressionStatement(let value):
         return value.expression.isPlaceholderNode()
       default:
         return false
